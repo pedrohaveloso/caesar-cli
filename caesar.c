fc21392a006322060f25d86a1a08fb09d6a57e34
Pedro Veloso <contatopedrohalves@gmail.com>
2024-05-07 10:26:29 -0300

    feat: create main file.

---
+++ caesar.c
@@ -0,0 +1,17 @@
+/**
+ * Caesar cipher cli encoder/decoder.
+ *
+ * - Default shift is 3.
+ *
+ * ## Examples:
+ *
+ *        $ caesar encode "hello world" => "khoor zruog"
+ *        $ caesar encode "hello world" -s 7 => "olssv dvysk"
+ *
+ *        $ caesar decode "khoor zruog" => "hello world"
+ *        $ caesar decode "olssv dvysk" -s 7 => "hello world"
+ */
+
+/**
+ * Print help message.
+ */
